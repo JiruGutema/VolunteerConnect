40346f92cf5ca2d70df8093f2b7f083c95848e13
JiruGutema <jethior1@gmail.com>
2025-05-04 16:51:19 +0300

    backend is done. folder structure for mvvp jetpack

--- backend/src/routes/application.route.js
+++ backend/src/routes/application.route.js
@@ -1,6 +1,17 @@
 const express = require("express");
 const router = express.Router();
-const { createEvent, getEventById, deleteEvent, updateEvent, getAllEvents, getOrganizationEvents, getUserEventApplications, getEventApplicants, applyForEvent } = require("../controllers/application.controller");
+const { 
+    createEvent, 
+    getEventById, 
+    deleteEvent, 
+    updateEvent, 
+    getAllEvents, 
+    getOrganizationEvents, 
+    getUserEventApplications, 
+    getEventApplicants, 
+    applyForEvent 
+
+} = require("../controllers/application.controller");
 const authMiddleware = require("../middlewares/authMiddleware");
 const adminMiddleware = require("../middlewares/adminMiddleware");
 
@@ -12,11 +23,13 @@ router.get("/events/org", getOrganizationEvents) // all events by organization
 router.get("/applications/:eventId", getEventById); // post details by id
 router.delete("/application/:eventId", deleteEvent); // delete event by id
 router.put("/application/update/:eventId", updateEvent);  // update event by id
-router.get("/application/getallapplications", getAllEvents);    // explorer
+
+router.get("/application/getallapplications",authMiddleware, getAllEvents);    // explore
 
 router.get("/events/:eventId/applicants/",authMiddleware, getEventApplicants); // get all applicants for an event
 
 router.get("/application/myApplication", getUserEventApplications) // get all applications by user
+
 router.post("/application/apply/:eventId",authMiddleware, applyForEvent) // apply for event
 
 module.exports = router;
